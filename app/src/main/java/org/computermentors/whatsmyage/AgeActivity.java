4eebe658028d2a237a6a3a137e47518e4124f7a0
CryoFlar3 <ndukes@computermentors.org>
2016-02-09 16:42:06 -0500

    Added widgets and buttons
    Buttons should go from one page to the other. Next need to transfer
    data from one page to the next.

--- app/src/main/java/org/computermentors/whatsmyage/AgeActivity.java
+++ app/src/main/java/org/computermentors/whatsmyage/AgeActivity.java
@@ -1,7 +1,11 @@
 package org.computermentors.whatsmyage;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 
 public class AgeActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class AgeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_age);
+
+        final DatePicker birthDatePicker = (DatePicker) findViewById(R.id.birthDatePicker);
+        final Button backButton = (Button) findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMain();
+            }
+        });
     }
+
+    private void startMain(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
 }
--- app/src/main/java/org/computermentors/whatsmyage/MainActivity.java
+++ app/src/main/java/org/computermentors/whatsmyage/MainActivity.java
@@ -1,7 +1,11 @@
 package org.computermentors.whatsmyage;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final DatePicker birthDatePicker = (DatePicker) findViewById(R.id.birthDatePicker);
+        final Button findAgeButton = (Button) findViewById(R.id.findAgeButton);
+
+        findAgeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAgeActivity();
+            }
+        });
+    }
+
+    private void startAgeActivity(){
+        Intent intent = new Intent(this, AgeActivity.class);
+        startActivity(intent);
     }
 }
