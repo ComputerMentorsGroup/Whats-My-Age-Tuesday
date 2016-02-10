41473be01609c644d492db2304f995b5ac9ffb80
CryoFlar3 <ndukes@computermentors.org>
2016-02-09 19:02:42 -0500

    Added functions
    Added functions for getting the date and year. Now to program it.

--- app/src/main/java/org/computermentors/whatsmyage/Calculate.java
+++ app/src/main/java/org/computermentors/whatsmyage/Calculate.java
@@ -1,7 +1,19 @@
 package org.computermentors.whatsmyage;
 
+import android.widget.DatePicker;
+
+import java.util.Date;
+
 /**
  * Created by CryoFlar3 on 2/9/16.
  */
 public class Calculate {
+
+    Date getDate (DatePicker date){
+        
+    }
+
+    int getAge (Date date){
+
+    }
 }
